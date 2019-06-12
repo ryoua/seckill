44e908e310f46832e649ff7b3db7b319d53be05d
ryoua <1794970183@qq.com>
2019-06-12 20:56:23 +0800

    Initial commit

---
+++ src/main/java/com/ryoua/seckill/entity/Seckill.java
@@ -0,0 +1,19 @@
+package com.ryoua.seckill.entity;
+
+import java.util.Date;
+
+/**
+ * 秒杀对应的实体
+ * @Author ryoua Created on 2019-06-12
+ */
+
+public class Seckill {
+    private long seckillId;
+    private String name;
+    private int number;
+    private Date startTime;
+    private Date endTime;
+    private Date createTime;
+
+
+}
