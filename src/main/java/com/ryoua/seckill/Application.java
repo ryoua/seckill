d0ba9c4b78451f47707d2753648a8bd44db9837c
ryoua <1794970183@qq.com>
2019-07-06 19:49:54 +0800

    完成初始化和配置文件的编写

---
+++ src/main/java/com/ryoua/seckill/Application.java
@@ -0,0 +1,14 @@
+package com.ryoua.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ryoua Created on 2019-07-06
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
