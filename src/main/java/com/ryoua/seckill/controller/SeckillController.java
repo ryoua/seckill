7a2b4c70de7ca8dc1c501bdac25a3dac2225505d
徐炳华 <31928820+ryoua@users.noreply.github.com>
2019-08-03 10:06:37 +0800

    浏览器静态缓存

--- src/main/java/com/ryoua/seckill/controller/SeckillController.java
+++ src/main/java/com/ryoua/seckill/controller/SeckillController.java
@@ -5,16 +5,20 @@ import com.ryoua.seckill.domain.SeckillOrder;
 import com.ryoua.seckill.domain.User;
 import com.ryoua.seckill.redis.RedisService;
 import com.ryoua.seckill.result.CodeMsg;
+import com.ryoua.seckill.result.Result;
 import com.ryoua.seckill.service.GoodsService;
 import com.ryoua.seckill.service.OrderService;
 import com.ryoua.seckill.service.SeckillService;
 import com.ryoua.seckill.service.UserService;
 import com.ryoua.seckill.vo.GoodsVo;
+import com.sun.org.apache.bcel.internal.classfile.Code;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Author: ryoua
@@ -39,30 +43,27 @@ public class SeckillController {
     @Autowired
     SeckillService seckillService;
 
-    @RequestMapping("/do_miaosha")
-    public String list(Model model, User user,
+    @RequestMapping(value = "/do_miaosha", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<OrderInfo> list(Model model, User user,
                        @RequestParam("goodsId")long goodsId) {
         model.addAttribute("user", user);
         if (user == null)
-            return "login";
+            return Result.error(CodeMsg.SESSION_ERROR);
         // 判断库存
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         int stock = goods.getStockCount();
         if (stock <= 0) {
-            model.addAttribute("errmsg", CodeMsg.MIAO_SHA_OVER.getMsg());
-            return "seckill_fail";
+            return Result.error(CodeMsg.MIAO_SHA_OVER);
         }
         // 判断是否秒杀到
         SeckillOrder order = orderService.getSeckillOrderByUserIdGoodsId(user.getId(), goodsId);
         if (order != null) {
-            model.addAttribute("errmsg", CodeMsg.REPEATE_MIAOSHA.getMsg());
-            return "seckill_fail";
+            return Result.error(CodeMsg.REPEATE_MIAOSHA);
         }
         // 秒杀
         OrderInfo orderInfo = seckillService.seckill(user, goods);
-        model.addAttribute("orderInfo", orderInfo);
-        model.addAttribute("goods", goods);
-        return "order_detail";
+        return Result.success(orderInfo);
     }
 
 }
